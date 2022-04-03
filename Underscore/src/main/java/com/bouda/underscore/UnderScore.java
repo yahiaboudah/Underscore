7e7507e39e3f3e8db3f721452f4903c0c323cc81
yahia boudah <yahiabouda@hotmail.com>
2022-04-03 18:53:27 +0100

    Underscore android lib

---
+++ Underscore/src/main/java/com/bouda/underscore/UnderScore.java
@@ -0,0 +1,36 @@
+package com.bouda.underscore;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.concurrent.Callable;
+
+public class UnderScore {
+
+    @SuppressWarnings("All")
+    public static <K, V> HashMap<K, V> _(K k, V v, Object... r)
+    {
+        List<Object> A = new ArrayList<>(Arrays.asList(r));
+        int L = A.size(), i = -2;
+        if(L % 2 == 1) A.remove(L---1);
+
+        HashMap<K, V> M = new HashMap<>();
+        M.put(k, v);
+        Object F, S;
+
+        while ((i += 2) < L){
+            F = A.get(i);
+            S = A.get(i+1);
+            // if(!(KClass.isInstance(F) /*&& VClass.isInstance(S) */)) continue;
+            M.put((K) F, (V) S);
+        }
+        return M;
+    }
+
+    public static <K> Callable<K>[] __(Callable<K>... args){ return args; }
+
+    public static <T> T[] __(T... args){
+        return args;
+    }
+}
